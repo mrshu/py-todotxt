2709c327097b6e981dc56485f207bfd464f41789
mr.Shu <mr@shu.io>
2014-08-23 12:10:02 +0200

    update: Added setup.cfg and setup.py
    * Added setup.cfg which sets the bdist_wheel parameter. In our case it
      should be `universal=1` as the code should be runnable on both py2 and
      py3
    
    * Added setup.py with all metadata required by PyPi

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='todotxt',
+    version='0.0.1',
+
+    description='A Python library for dealing with todo.txt files',
+    long_description=long_description,
+
+    url='https://github.com/mrshu/py-todotxt',
+
+    author='mrshu',
+    author_email='mr@shu.io',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='todotxt',
+    packages=['todotxt']
+)
+
